ce2e84a5d00c28e00b3c5697fd337da5536fdae9
Rafael Abdullah <rabdu59@wgu.edu>
2025-03-10 18:01:46 -0700

    ship

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+import { GlobalProvider } from "@/context/GlobalContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/assets/styles/globals.css";
+import "photoswipe/dist/photoswipe.css";
+
+export const metadata = {
+  title: "NestnRest",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <GlobalProvider>
+        <html>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </GlobalProvider>
+    </AuthProvider>
+  );
+};
+
+export default MainLayout;
